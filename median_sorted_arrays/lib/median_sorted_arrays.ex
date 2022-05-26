c993c02a7cd72cfc22e2c30b0009c36efb938be1
Samuel Monroe <spat.monroe@gmail.com>
2022-05-26 08:19:30 +0200

    Shitty traversal solution

--- median_sorted_arrays/lib/median_sorted_arrays.ex
+++ median_sorted_arrays/lib/median_sorted_arrays.ex
@@ -1,18 +1,47 @@
 defmodule MedianSortedArrays do
-  @moduledoc """
-  Documentation for `MedianSortedArrays`.
-  """
+  @spec find_median_sorted_arrays(nums1 :: [integer], nums2 :: [integer]) :: float
+  def find_median_sorted_arrays([], []), do: 0
+  def find_median_sorted_arrays(nums1, nums2) do
+    total_length = length(nums1) + length(nums2)
+    is_odd = rem(total_length, 2) == 1
+    target = case is_odd do
+      true -> div(total_length, 2)
+      false -> div(total_length, 2) - 1
+    end
 
-  @doc """
-  Hello world.
+    find_median_sorted_arrays(nums1, nums2, 0, 0, target, is_odd)
+  end
 
-  ## Examples
+  defp find_median_sorted_arrays(nums1, nums2, ix, iy, target, is_odd) when ix + iy == target and is_odd do
+    min(Enum.at(nums1, ix), Enum.at(nums2, iy))
+  end
 
-      iex> MedianSortedArrays.hello()
-      :world
+  defp find_median_sorted_arrays(nums1, nums2, ix, iy, target, _is_odd) when ix + iy == target do
+    next_nums1 = Enum.at(nums1, ix)
+    next_nums2 = Enum.at(nums2, iy)
 
-  """
-  def hello do
-    :world
+    if next_nums1 < next_nums2 do
+      (next_nums1 + min(Enum.at(nums1, ix+1), Enum.at(nums2, iy))) / 2
+    else
+      if next_nums1 > next_nums2 do
+        (next_nums2 + min(Enum.at(nums1, ix), Enum.at(nums2, iy+1))) / 2
+      else
+        (next_nums1 + next_nums2) / 2
+      end
+    end
+  end
+
+  defp find_median_sorted_arrays(nums1, nums2, ix, iy, target, is_odd) do
+    next_nums1 = Enum.at(nums1, ix)
+    next_nums2 = Enum.at(nums2, iy)
+    if next_nums1 < next_nums2 do
+      find_median_sorted_arrays(nums1, nums2, ix+1, iy, target, is_odd)
+    else
+      if next_nums1 > next_nums2 do
+        find_median_sorted_arrays(nums1, nums2, ix, iy+1, target, is_odd)
+      else
+        find_median_sorted_arrays(nums1, nums2, ix+1, iy, target, is_odd)
+      end
+    end
   end
 end
--- median_sorted_arrays/test/median_sorted_arrays_test.exs
+++ median_sorted_arrays/test/median_sorted_arrays_test.exs
@@ -1,8 +1,19 @@
 defmodule MedianSortedArraysTest do
   use ExUnit.Case
-  doctest MedianSortedArrays
 
-  test "greets the world" do
-    assert MedianSortedArrays.hello() == :world
+  test "case 1" do
+    assert MedianSortedArrays.find_median_sorted_arrays([1,3],[2]) == 2
+  end
+
+  test "case 2" do
+    assert MedianSortedArrays.find_median_sorted_arrays([1,2],[3,4]) == 2.5
+  end
+
+  test "case 3" do
+    assert MedianSortedArrays.find_median_sorted_arrays([],[1]) == 1
+  end
+
+  test "case 4" do
+    assert MedianSortedArrays.find_median_sorted_arrays([1,3],[2,7]) == 2.5
   end
 end
